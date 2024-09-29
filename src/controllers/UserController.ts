a08f4d325f313ad08c6fe101c27de5cee70afb99
mrs <mrs@debian>
2024-09-29 19:11:17 -0300

    Atualizando fullName()

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -3,23 +3,39 @@ import User from '../schemas/User';
 
 class UserController {
     
-    // Buscar usuários no MongoDB
+    // Users + FullName
     public async index(req: Request, res: Response): Promise<Response> {
         
         try {
-            const users = await User.find();
+            const users = await User.find()
 
-            return res.json(users);
-        }  catch (error) {
-            return res.status(500).json({ error: 'Erro ao buscar usuários' });
+            const usersWithFullName = users.map(user => {
+                const userObject = user.toObject()
+                return {
+                    ...userObject,
+                    fullName: user.fullName()
+                };
+            });
+
+            return res.json(usersWithFullName)
+        } catch (error) {
+            return res.status(500).json({ error: 'Erro ao buscar usuários' })
         }
     }
 
-    // Inserir usuários no MOngoDB
+    // Inserir usuários no MongoDB
     public async store (req: Request, res: Response): Promise<Response> {
-        const user = await User.create(req.body)
+        try {
+            const user = await User.create(req.body)
+            const userObject = user.toObject()
+            return res.json({
+                ...userObject,
+                fullName: user.fullName() 
+            })
 
-        return res.json(user)
+        } catch (error) {
+            return res.status(500).json({ error: 'Erro ao inserir usuário' })
+        }
     }
 }
 
